1d9d28ea77b0a90d230cd02393dfe62604cc9496
root <root@apoorva.in>
2022-02-23 09:12:03 -0500

    First version of our tool

---
+++ achilles.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+import validators
+import yaml
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from bs4 import Comment
+
+parser = argparse.ArgumentParser(description='The Achilles HTML Vulnerability Analyser Version 1.0')
+
+parser.add_argument('-v','--version',action='version', version='%(prog)s 1.0')
+parser.add_argument('url', type=str, help="THe URL of the HTML to analyze")
+parser.add_argument('--config', help='path to configuration file')
+parser.add_argument('-o', '--output', help='Report file to output path')
+
+args=parser.parse_args()
+
+config = {'forms': True,'comments': True,'password_inputs':True}
+
+if(args.config):
+   print('using config file: ' +args.config)
+   config_file = open(args.config, 'r')
+   config_form_file = yaml.load(config_file)
+   if(config_form_file):
+      config = {**config, **config_form_file}
+
+report =''
+
+url=args.url
+
+if(validators.url(url)):
+   result_html= requests.get(url).text
+   parsed_html= BeautifulSoup(result_html, 'html.parser')
+
+   forms = (parsed_html.find_all('form'))
+   comments = parsed_html.find_all(string=lambda text:isinstance(text,Comment))
+   password_inputs = parsed_html.find_all('input',{'name':'password'})
+
+   if(config['forms']):
+     for form in forms:
+       if((form.get('action').find('https') < 0) and (urlparse(url).scheme!='https')):
+            report+= 'Form Issue: Insecure form action ' + form.get('action')+ ' found in document\n'
+   if(config['comments']):
+     for comment in comments:
+       if(comment.find('key:') > -1):
+            report+='Comment Issue: Key is found in HTML comments please remove\n'
+   if(config['password_inputs']):
+     for passwordinput in password_inputs:
+       if(passwordinput.get('type') != 'password'):
+            report+='Password Issue: Plaintext password input found. Please change to password type input\n'
+else:
+	print("that wasn't a good url")
+
+if (report == ''):
+	report +='Nice job: your html document is secure!\n'
+else:
+	header = 'Vulnerability Report is as follows: \n'
+	header += '==================================== \n\n'
+	report= header+report
+
+print(report)
+
+if(args.output):
+    f = open(args.output, 'w')
+    f.write(report)
+    f.close
+    print('Report saved to: ' + args.output)
+
